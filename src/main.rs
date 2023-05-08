8ab7e99daa312143d853e35d342847067e8bb63a
Steggy <mjsteger1@gmail.com>
2023-05-08 15:27:29 -0700

    update: getting curl working, not yet able to ping server

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::*;
 use pnet::packet::ethernet::*;
 use pnet::packet::*;
+use pnet::packet::icmp::*;
+use pnet::packet::icmp::echo_reply::*;
 
 #[allow(unused_variables, unused_imports, unused_mut)]
 use std::net::{TcpListener, TcpStream};
-use std::*;
+use std::{*, io::Read};
 
+#[allow(clippy::unused_io_amount)]
 fn main() -> std::io::Result<()> {
     let interface_name = env::args().nth(1).unwrap();
     let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
@@ -16,8 +19,7 @@ fn main() -> std::io::Result<()> {
     let interfaces = datalink::interfaces();
     let interface = interfaces
         .into_iter()
-        .filter(interface_names_match)
-        .next()
+        .find(interface_names_match)
         .unwrap();
 
     println!("Interface is {:?}", interface.to_string());
@@ -29,16 +31,23 @@ fn main() -> std::io::Result<()> {
             e
         ),
     };
+    let listener = TcpListener::bind("127.0.0.2:7").unwrap();
 
-    loop {
-        match rx.next() {
-            Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-                println!("Packet: Source {:?}", packet.payload());
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut obj) => {
+                let mut buf = [0;1024];
+                obj.read(&mut buf)?;
+                let mut pack = EchoReplyPacket::new(&mut buf).unwrap();
+                let icmp_packet = IcmpPacket::new(&mut pack.payload()).unwrap();
+                println!("Packet looks like: {:?}", icmp_packet);
+                println!("Packet looks like: {:?}", icmp_packet.get_icmp_type());
+
+                println!("{:?}", str::from_utf8(&buf))
             }
             Err(e) => {
-                eprint!("Error: {:?}", e);
             }
         }
     }
+    Ok(())
 }
